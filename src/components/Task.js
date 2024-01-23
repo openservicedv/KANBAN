2391c2fa7c7cb2d567806ac49d24294073e58c57
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-01-22 20:33:29 -0800

    3

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import {Button} from "reactstrap";
+import Button from 'react-bootstrap/Button';
 
 const Task = ({task}) => {
     return (
@@ -9,31 +11,72 @@ const Task = ({task}) => {
              }}>
             <div className="card-body"
                  style={{
-                     border: "solid brown",
+                     border: "brown dashed",
                      marginBottom: "10px",
                      height: "300px",
 
                  }}>
-                <h5 className="card-title"
-                    style={{
-                        border: "solid red",
+                <div className="d-flex justify-content-start"
+                     style={{
+                         border: "dashed blue",
+                         marginBottom: "5px",
 
-                    }}>{task.name}</h5>
+                     }}>
+                    <h5 className="card-title"
+                        style={{
+                            border: "solid red",
+
+                        }}>{task.name}</h5>
+                    <Button variant="outline-secondary">
+                        -
+                    </Button>
+
+                </div>
                 <p className="card-text"
                    style={{
                        border: "solid green",
 
                    }}>{task.description}</p>
-                <p className="card-text"
-                   style={{
-                       border: "solid blue",
+                <div className="d-flex justify-content-start"
+                     style={{
+                         border: "dashed green",
+                         marginBottom: "5px",
+                     }}
+                >
+                    <p className="card-text"
+                       style={{
+                           border: "solid blue",
 
-                   }}>priority: {task.priority}</p>
-                <p className="card-text"
-                   style={{
-                       border: "solid red",
+                       }}>priority: {task.priority}
+                    </p>
+                    <Button variant="outline-secondary">
+                        -
+                    </Button>
+                    <Button variant="outline-secondary">
+                        +
+                    </Button>
+                </div>
+                <div className="d-flex justify-content-start"
+                     style={{
+                         border: "dashed red",
 
-                   }}>priority: {task.status}</p>
+                     }}
+                >
+                    <Button variant="outline-secondary">
+                        -
+                    </Button>
+                    <Button variant="outline-secondary">
+                        Delete
+                    </Button>
+                    <Button variant="outline-secondary">
+                        +
+                    </Button>
+                </div>
+                {/*<p className="card-text"*/}
+                {/*   style={{*/}
+                {/*       border: "solid red",*/}
+
+                {/*   }}>priority: {task.status}</p>*/}
             </div>
         </div>
     );
