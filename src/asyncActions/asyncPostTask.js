87bf4766e342d654735590cbea087947c14ac1f9
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-01-24 20:32:43 -0800

    full redux 1

--- src/asyncActions/asyncPostTask.js
+++ src/asyncActions/asyncPostTask.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import {asyncGetTasks} from "./asyncGetTasks";
-
 export const asyncPostTask = (newTask) => {
     return function (dispatch) {
         axios.post(`${process.env.REACT_APP_BASE_URL}/tasks`, newTask)
